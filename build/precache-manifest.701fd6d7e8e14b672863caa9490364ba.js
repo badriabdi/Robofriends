ab6dc58d26a3d2ef7a804e964d27949c90950910
badri m abdi <badriabdi877@gmail.com>
2019-11-06 05:39:46 -0700

    added robofriends

---
+++ build/precache-manifest.701fd6d7e8e14b672863caa9490364ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5014413d94da1486371616854f710eb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c1bcef0dab6ce3c20b6",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ddfa392905ac5cf5a05f",
+    "url": "/static/css/main.dee5a4ef.chunk.css"
+  },
+  {
+    "revision": "0c1bcef0dab6ce3c20b6",
+    "url": "/static/js/2.d75c0efc.chunk.js"
+  },
+  {
+    "revision": "ddfa392905ac5cf5a05f",
+    "url": "/static/js/main.db4a49a5.chunk.js"
+  },
+  {
+    "revision": "c332e1bb67579b93fb1f",
+    "url": "/static/js/runtime-main.b75238ee.js"
+  }
+]);
---
+++ src/container/App.js
@@ -0,0 +1,62 @@
+import React,{Component} from 'react';
+import CardList from '../component/CardList';
+import SearchBox from '../component/SearchBox';
+import './App.css';
+import Scroll from '../component/Scroll'
+
+class App extends Component  {
+	constructor(){
+		super()
+		this.state = {
+			robots:[],
+			Searchfield: ''
+		}
+	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users').then(response=>{
+			return response.json();
+		})
+		.then(users =>{
+			this.setState({robots: users})
+		});
+		
+	}
+	onSearchChange = (event)=>{
+		this.setState ({Searchfield:event.target.value})
+
+	}
+
+	render(){
+		const {robots,Searchfield} = this.state;
+		const filteredrobots = robots.filter(robot => {
+		return robot.name.toLowerCase().includes(Searchfield.toLowerCase())
+
+		})
+
+	 if (!robots.length){
+	 	return <h1>loading</h1>
+	 }else{
+
+	 	return (
+		<div className='tc'>
+		<h1 className='f1'>Robotfriends</h1>
+		<SearchBox searchChange={this.onSearchChange}/>
+		<Scroll>
+		< CardList robots={filteredrobots}/>
+		</Scroll>
+		</div>
+		)
+	}
+}
+
+	 }
+
+	
+
+
+export default App;
+
+
+												
+							
