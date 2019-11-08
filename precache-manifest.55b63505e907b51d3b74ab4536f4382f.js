9327255e790cbd6ddc25c849bd4c8654004f3346
badri m abdi <badriabdi877@gmail.com>
2019-11-08 09:11:07 -0700

    Updates

---
+++ precache-manifest.55b63505e907b51d3b74ab4536f4382f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f00fff53327ed21488ae3ec446ecd8a",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0ccd3156554411fdbf9a",
+    "url": "/Robofriends/static/css/main.dee5a4ef.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/Robofriends/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "0ccd3156554411fdbf9a",
+    "url": "/Robofriends/static/js/main.2d62da2d.chunk.js"
+  },
+  {
+    "revision": "78cab9b9619ddcd920c7",
+    "url": "/Robofriends/static/js/runtime-main.ff6ebadc.js"
+  }
+]);
