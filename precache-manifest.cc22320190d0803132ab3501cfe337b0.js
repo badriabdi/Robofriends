35eed1d6e9393e750ab7589e432da71882eba97d
badri m abdi <badriabdi877@gmail.com>
2019-11-13 19:59:20 -0700

    Updates

---
+++ precache-manifest.cc22320190d0803132ab3501cfe337b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da3ac6ddf58a531ed3a295a7624cdd1",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4e518fdbc7b0fcc6c7b6",
+    "url": "/Robofriends/static/css/main.dee5a4ef.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/Robofriends/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "4e518fdbc7b0fcc6c7b6",
+    "url": "/Robofriends/static/js/main.f661ba22.chunk.js"
+  },
+  {
+    "revision": "78cab9b9619ddcd920c7",
+    "url": "/Robofriends/static/js/runtime-main.ff6ebadc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.2a5c6292b6669af4128329cf6eacf010.js"
+  "/Robofriends/precache-manifest.cc22320190d0803132ab3501cfe337b0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Robofriends/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
